31fd2e5d75c41e0cdfea53d6fc522796f968b634
pradivgnanaraj <tpradivgnanaraj@gmail.com>
2023-07-24 14:31:16 +0530

    Access data's title and description

--- main.py
+++ main.py
@@ -3,8 +3,17 @@
 import requests
 
 api_key = "de4b7cde6e684f208f9d6599deff6d3f"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2023-06-24&sortBy=publishedAt&apiKey=de4b7cde6e684f208f9d6599deff6d3f"
+url = "https://newsapi.org/v2/everything?q=tesla&from=2023-06-24&"\
+    "sortBy=publishedAt&apiKey="\
+    "de4b7cde6e684f208f9d6599deff6d3f"
 
+# Make request
 request = requests.get(url)
-content = request.text
-print(content)
+
+# Set a dictionary with data
+content = request.json()
+
+# Access the article titles and description
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
