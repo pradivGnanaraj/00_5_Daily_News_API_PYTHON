604666eeb50012310f7402467d637e87ee69bc71
pradivgnanaraj <tpradivgnanaraj@gmail.com>
2023-07-24 15:47:07 +0530

    Bug Fix: RFC 5322

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from send_email import send_email
 import requests
 
-topic = tesla
+topic = "tesla"
 api_key = "de4b7cde6e684f208f9d6599deff6d3f"
 url = f"https://newsapi.org/v2/everything?q={topic}&" \
         "from=2023-06-24&" \
@@ -19,10 +19,12 @@ content = request.json()
 # Access the article titles and description
 body = ""
 for article in content["articles"][:20]:
-    if article["title"] is not None:
-        body = "Subject: Today's news" + body + article["title"] + "\n" + article["description"] + \
-               "\n" + article["url"] + 2*"\n"
 
+    if article["title"] is not None:
+        body = "Subject: Today's News" \
+               + "\n" + body + article["title"] + "\n" \
+               + article["description"] \
+               + "\n" + article["url"] + 2*"\n"
 
 body = body.encode("utf-8")
 send_email(message=body)
